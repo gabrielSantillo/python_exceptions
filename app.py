07599573f94f46bfd5ad890f682e91c52f77cc71
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-22 20:54:43 -0600

    using recursive exceptions to fix errors

--- app.py
+++ app.py
@@ -9,3 +9,22 @@ except:
     print("uh oh! Call Helppppppp")
 finally:
     print("You've tried something neat")
+
+
+def asking_age():
+    age = input("How old are you? ")
+    try:
+        age = float(age)
+    except ValueError:
+        print("You should type only numbers.")
+        age = asking_age()
+    except:
+        print("uh oh")
+
+    return age
+
+
+result_one = asking_age()
+result_two = asking_age()
+
+print(result_one, result_two)
